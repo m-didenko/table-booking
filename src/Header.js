2834fe28b8501664f238d8777ca7c4a6b0ee2923
Nick Didenko <Nick@Mykytas-MacBook-Air.local>
2024-12-18 20:41:48 -0600

    responsive styles and burger menu

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,36 @@
 import logo from './assets/logo.svg';
 
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './Header.css';
+
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <header>
-            <img src={logo} alt="Little Lemon Logo" />
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="Little Lemon Logo" />
+            </div>
+            <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#reservations">Reservations</a></li>
+                    <li><a href="#order-online">Order Online</a></li>
+                    <li><a href="#login">Login</a></li>
+                </ul>
+            </nav>
+            <button className="burger" onClick={toggleMenu}>
+                {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
         </header>
     );
 }
+
 export default Header;
