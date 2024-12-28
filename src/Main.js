0641ceef27f21c99b5d4b3c433adc574c4ab115a
Nick <mykytadid@gmail.com>
2024-12-28 15:16:56 -0600

    style fixes

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,4 @@
 import restFood from './assets/restaurantfood.jpg';
-import bruschetta from './assets/bruchetta.svg'
-import greekSalad from './assets/greek-salad.jpg'
-import lemonDessert from './assets/lemon-dessert.jpg'
-import person1 from './assets/person1.png'
-import person2 from './assets/person2.png'
-import person3 from './assets/person3.png'
-import person4 from './assets/person4.png'
-import Star from './components/Star'
 import About from "./components/About";
 import Specials from "./components/Specials";
 import PeopleSay from "./components/PeopleSay";
@@ -27,13 +19,9 @@ function Main() {
                     </div>
                 </div>
             </section>
-
             <Specials />
             <PeopleSay />
-
-            <section>
-                <About />
-            </section>
+            <About />
         </main>
     );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -7,22 +7,24 @@ import './About.css'
 
 const About = () => {
     return (
-        <div className="about-container">
-            <div className="about-text-section">
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                </p>
-                <p>
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-                </p>
+        <section>
+            <div className="about-container">
+                <div className="about-text-section">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                    <p>
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                    </p>
+                </div>
+                <div className="about-image-section">
+                    <img src={about1} alt="Chefs talking in kitchen" />
+                    <img src={about2} alt="Kitchen interior" />
+                </div>
             </div>
-            <div className="about-image-section">
-                <img src={about1} alt="Chefs talking in kitchen" />
-                <img src={about2} alt="Kitchen interior" />
-            </div>
-        </div>
+        </section>
     );
 };
 
